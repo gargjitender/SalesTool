c868118e6e87482796ab4f5ee3a27ec3550c3200
5220344 <jitender.garg.osv@fedex.com>
2019-03-07 14:37:55 -0600

    Added the second output

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -14,5 +14,6 @@ public class Main {
 		System.out.println("Display the Greeting Card");
 		System.out.println("Happyy Sales People");
 		System.out.println("Testing some more thing");
+		System.out.println("Testing 2");
 	}
 }
